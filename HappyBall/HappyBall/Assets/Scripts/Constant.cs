f705ef72fe25822b870d465d840aad0ba26e3d79
lixianhua_pro <865055917_pro@qq.com>
2020-12-24 15:19:06 +0800

    change name

---
+++ HappyBall/HappyBall/Assets/Scripts/Constant.cs
@@ -0,0 +1,9 @@
+﻿
+public class Constant {
+    public const string Skin = "Skin";
+    public const string Mode = "Mode";
+    public const string LastScore = "LastScore";
+    public const string BestScore = "BestScore";
+    public const string LastLength = "LastLength";
+    public const string BestLength = "BestLength";
+}
