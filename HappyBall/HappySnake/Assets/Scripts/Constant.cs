e1630acf46f754d3f5a723a2582b42fd04300256
lixianhua_pro <865055917_pro@qq.com>
2020-12-25 15:47:00 +0800

    目录修改

--- HappyBall/HappySnake/Assets/Scripts/Constant.cs
+++
@@ -1,9 +0,0 @@
-﻿
-public class Constant {
-    public const string Skin = "Skin";
-    public const string Mode = "Mode";
-    public const string LastScore = "LastScore";
-    public const string BestScore = "BestScore";
-    public const string LastLength = "LastLength";
-    public const string BestLength = "BestLength";
-}
